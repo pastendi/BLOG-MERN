1af353cf36c43a3e4d843a225a080ac21c85596b
Tendi <pastendisherpa26@gmail.com>
2023-05-17 22:46:10 +0545

    addition of mobile menu and responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,12 @@ import { NavLink } from 'react-router-dom'
 import logo from '../assets/logo.png'
 import { BsCloudMoonFill, BsFillSunFill } from 'react-icons/bs'
 import { HiMenu } from 'react-icons/hi'
+import { AiOutlineClose } from 'react-icons/ai'
 import { navLinks } from '../utils/constant'
+import { useState } from 'react'
 
 const Navbar = ({ theme, toggleTheme }) => {
+  const [menuClicked, setMenuClicked] = useState(false)
   return (
     <div className='fixed top-0 left-0 w-full blur-background'>
       <nav className='container max-w-6xl h-20 mx-auto flex items-center px-2 sm:px-6 '>
@@ -40,7 +43,7 @@ const Navbar = ({ theme, toggleTheme }) => {
         {/* right */}
         <div className='hidden sm:flex space-x-2'>
           <button
-            className='block px-3 py-1 border-sky-400 shadow-sky-300 shadow-sm border-1 bg-darkBlue rounded-lg'
+            className='block px-3 py-3 border-sky-400 shadow-sky-300 shadow-sm border-1 bg-darkBlue rounded-lg'
             onClick={toggleTheme}
           >
             {theme === 'dark' ? (
@@ -54,9 +57,51 @@ const Navbar = ({ theme, toggleTheme }) => {
           </button>
         </div>
         <div className='block sm:hidden'>
-          <HiMenu size={40} />
+          {menuClicked ? (
+            <div onClick={() => setMenuClicked(false)}>
+              <AiOutlineClose size={40} />
+            </div>
+          ) : (
+            <div onClick={() => setMenuClicked(true)}>
+              <HiMenu size={40} />
+            </div>
+          )}
         </div>
       </nav>
+      {/* mobile menu */}
+      {menuClicked && (
+        <div className='flex flex-col sm:hidden space-y-4 pb-6'>
+          {navLinks.map((x, index) => {
+            return (
+              <NavLink
+                to={x.path}
+                key={index}
+                onClick={() => setMenuClicked(false)}
+                className={({ isActive }) =>
+                  isActive ? 'nav-link border-r-4' : 'nav-link'
+                }
+              >
+                {x.link}
+              </NavLink>
+            )
+          })}
+          <div className='flex items-center justify-center space-x-4'>
+            <button
+              className='block px-3 py-3 border-sky-400 shadow-sky-300 shadow-sm border-1 bg-darkBlue rounded-lg'
+              onClick={toggleTheme}
+            >
+              {theme === 'dark' ? (
+                <BsFillSunFill size={20} />
+              ) : (
+                <BsCloudMoonFill size={20} color='white' />
+              )}
+            </button>
+            <button className=' block btn bg-info hover:bg-infoDark'>
+              Login
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
